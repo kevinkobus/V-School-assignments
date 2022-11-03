8ffbc0b1d68090a424e176233860f2df0256e636
Kevin Kobus <kevinkobus@gmail.com>
2022-11-03 13:45:42 -0600

    finished array sort exercises

--- Level-2/array-sort-exercises/app.js
+++ Level-2/array-sort-exercises/app.js
@@ -1,44 +1,66 @@
 // 1) Sort an array from smallest number to largest
 function leastToGreatest(arr) {
-    // your code here
-  }
+      arr.sort(function(a, b){
+    return a - b
+  })
+  return arr
+}
   
   console.log(leastToGreatest([1, 3, 5, 2, 90, 20])); // [1, 2, 3, 5, 20, 90]
   
-  
   // 2) Sort an array from largest number to smallest
-  // function greatestToLeast(arr) {
-  //   // your code here
-  // }
-  
-  // console.log(greatestToLeast([1, 3, 5, 2, 90, 20])); // [90, 20, 5, 3, 2, 1]
+  function greatestToLeast(arr) {
+    arr.sort(function(a, b){
+      return b - a
+  })
+    return arr
+  }
+  console.log(greatestToLeast([1, 3, 5, 2, 90, 20])); // [90, 20, 5, 3, 2, 1]
   
   // // 3) Sort an array from shortest string to longest
-  // function lengthSort(arr) {
-  //   // your code here
-  // }
+  function lengthSort(arr) {
+    arr.sort(function(a, b){
+      return a.length - b.length
+    })
+    return arr
+  }
   
-  // console.log(lengthSort(["dog", "wolf", "by", "family", "eaten"])); // ["by", "dog", "wolf", "eaten", "family"]
+  console.log(lengthSort(["dog", "wolf", "by", "family", "eaten"])); // ["by", "dog", "wolf", "eaten", "family"]
   
   // // 4) Sort an array alphabetically
-  // function alphabetical(arr) {
-  
-  // }
-  
-  // console.log(alphabetical(["dog", "wolf", "by", "family", "eaten"])); // ["by", "dog", "eaten", "family", "wolf"]
+  function alphabetical(arr) {
+    arr.sort(function(a, b){
+      if(a > b){
+        return 1
+      }
+      else return -1
+    })
+   return arr
+  }
+
+  //don't quite understand this way but it works
+  // return arr.sort((a, b) => a[0].localeCompare(b[0]))
+
+  console.log(alphabetical(["dog", "wolf", "by", "family", "eaten"])); // ["by", "dog", "eaten", "family", "wolf"]
   
   
   // // 5) Sort the objects in the array by age
-  // function byAge(arr){
-  //   // your code here
-  // }
+  function byAge(arr){
+    arr.sort(function (a, b){
+      if (a.age > b.age){
+        return 1
+      }
+      else return -1
+    })
+    return arr
+  }
   
-  // console.log(byAge([
-  //     { name: "Quiet Samurai", age: 22 },
-  //     { name: "Arrogant Ambassador", age: 100 },
-  //     { name: "Misunderstood Observer", age: 2 },
-  //     { name: "Unlucky Swami", age: 77 }
-  // ]));
+  console.log(byAge([
+      { name: "Quiet Samurai", age: 22 },
+      { name: "Arrogant Ambassador", age: 100 },
+      { name: "Misunderstood Observer", age: 2 },
+      { name: "Unlucky Swami", age: 77 }
+  ]));
   // // => [ { name: 'Misunderstood Observer', age: 2 },
   // //  { name: 'Quiet Samurai', age: 22 },
   // //  { name: 'Unlucky Swami', age: 77 },
