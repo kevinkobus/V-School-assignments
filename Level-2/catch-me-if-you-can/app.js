0c9bad71420a1c0893a14c3322416585b802c6be
Kevin Kobus <kevinkobus@gmail.com>
2022-11-06 14:01:28 -0700

    completed catch me if you can assignment

---
+++ Level-2/catch-me-if-you-can/app.js
@@ -0,0 +1,110 @@
+// 1a) Write a function that returns the sum of two numbers. Throw an error if either argument is not of the data type number:
+function sum(x, y) {
+    try {
+        if (typeof x != "number" || typeof y != "number") {
+            throw new Error("Please enter a number only")
+        }
+        return x + y
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+
+console.log(sum(6, 9))
+
+// 1b) Call the sum function inside a try block using "1" and "2" as arguments. Use console.log within a catch block to inform the user of the error.
+function sum(x, y) {
+    try {
+        if (typeof x != "number" || typeof y != "number") {
+            throw new Error("Please enter a number only")
+        }
+        return x + y
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+
+try {
+    sum(1, 2)
+}
+catch(err){
+    console.log(err)
+}
+
+// 2a) Given a user object, write a function called login that takes a username and password as parameters. Throw an error if either of them don't match. Otherwise, log to the console a message saying "login successful!"
+
+var user = { username: "sam", password: "123abc" }
+function login(username, password) {
+    try {
+        if (username != user.username || password != user.password) {
+            throw new Error("Invalid username or password, please try again")
+        }
+        console.log("login successful!")
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+console.log(login("sam", "123abc"))
+
+// 2b) Call the login function within a try block. In one instance use the correct credentials, and in another use incorrect ones. Make sure you see the appropriate message!
+var user = { username: "sam", password: "123abc" }
+function login(username, password) {
+    try {
+        if (username != user.username || password != user.password) {
+            throw new Error("Invalid username or password, please try again")
+        }
+        console.log("login successful!")
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+try{
+    login("sam", "123abc")
+}
+catch(err){
+    console.log(err)
+}
+
+//correct credentials
+var user = { username: "sam", password: "123abc" }
+function login(username, password) {
+    try {
+        if (username != user.username || password != user.password) {
+            throw new Error("Invalid username or password, please try again")
+        }
+        console.log("login successful!")
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+try{
+    login("sam", "123abc")
+}
+catch(err){
+    console.log(err)
+}
+
+//incorrect credentials
+var user = { username: "sam", password: "123abc" }
+function login(username, password) {
+    try {
+        if (username != user.username || password != user.password) {
+            throw new Error("Invalid username or password, please try again")
+        }
+        console.log("login successful!")
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+try{
+    login("ted", "123abc")
+}
+catch(err){
+    console.log(err)
+}
