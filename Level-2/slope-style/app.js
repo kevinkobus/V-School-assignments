c9c6d2a1bdf82c7e67d42efde8bc095a147323cf
Kevin Kobus <kevinkobus@gmail.com>
2022-11-19 15:26:38 -0700

    set up folder for the princess is in another castle

--- Level-2/slope-style/app.js
+++ Level-2/slope-style/app.js
@@ -6,8 +6,8 @@
 // }
 
 function collectAnimals(...arr) {
-   return arr
- }
+  return arr;
+}
 
 console.log(collectAnimals("dog", "cat", "mouse", "jackolope", "platypus"));
 // ["dog", "cat", "mouse", "jackolope", "platypus"]
@@ -31,3 +31,90 @@ console.log(combineFruit(["apple", "pear"], ["cake", "pie"], ["carrot"]));
 //         sweets: ["cake", "pie"],
 //         vegetables: ["carrot"]
 //      }
+
+// Use destructuring to use the property names as variables. Destructure the object in the parameter:
+
+// - Hint
+// - This function is expecting two variables to be defined in the parameter list.
+
+// function parseSentence(_________){
+//   return `We're going to have a good time in ${location} for ${duration}`
+// }
+
+function parseSentence({ location, duration }) {
+  return `We're going to have a good time in ${location} for ${duration}`;
+}
+
+console.log(
+  parseSentence({
+    location: "Burly Idaho",
+    duration: "2 weeks",
+  })
+);
+
+// Use array destructuring to make this code ES6:
+
+// function returnFirst(items){
+//     const firstItem = items[0]; /*change this line to be es6*/
+//     return firstItem
+// }
+
+function returnFirst(items) {
+  const [firstItem] = items;
+  return firstItem;
+}
+
+console.log(returnFirst(["bat", "ball", "glove"]));
+
+// Write destructuring code to assign variables that will help us return the expected string. Also, change the string to be using Template literals:
+
+const favoriteActivities = [
+  "magnets",
+  "snowboarding",
+  "philanthropy",
+  "janitor work",
+  "eating",
+];
+
+// function returnFavorites(arr){
+//     /*your code here*/
+//     return "My top three favorite activities are, " + firstFav + ", " + secondFav + ", and " + thirdFav";
+// }
+
+function returnFavorites(arr) {
+  const [firstFav, secondFav, thirdFav] = arr;
+  return "My top three favorite activities are, ${firstFav}, ${secondFav}, and ${thirdFav}";
+}
+
+console.log(returnFavorites(favoriteActivities));
+
+
+// Blue Square
+// Use the Rest and Spread Operator to help take any number of arrays, and return one array. You will need to change how the arrays are passed in. You may write it assuming you will always recieve three arrays if you would like to.
+
+// function combineAnimals() {
+
+// }
+
+const realAnimals = ["dog", "cat", "mouse"];
+const magicalAnimals = ["jackolope"];
+const mysteriousAnimals = ["platypus"];
+
+function combineAnimals(array, ...moreArrays) {
+ return [...moreArrays,...array]
+}
+
+console.log(combineAnimals(realAnimals, magicalAnimals, mysteriousAnimals));
+
+// ["dog", "cat", "mouse", "jackolope", "platypus"]
+
+// Black Diamonds
+// Try to make the following function more ES6y:
+
+// function product(a, b, c, d, e) {
+//   var numbers = [a,b,c,d,e];
+
+//   return numbers.reduce(function(acc, number) {
+//     return acc * number;
+//   }, 1)
+// }
