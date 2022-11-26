df5350ecefcb235d86a27687439170906d424d2a
Kevin Kobus <kevinkobus@gmail.com>
2022-11-26 13:19:18 -0700

    more progress made on the princess is in another castle

--- Level-2/princess-another-castle/app.js
+++ Level-2/princess-another-castle/app.js
@@ -1,20 +1,7 @@
 const readline = require("readline-sync");
 
-function randomInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-//   console.log(randomInteger(0, 2))
-
-//pick player question
-const pickPlayer = readline.keyIn("Hello, would you like to be 'm' = Mario or 'l' = Luigi? ");
-
-// instantiate player
-const activePlayer = new Player({name: activePlayer.name, status: "Big", totalCoins: 0, hasStar: false });
-
 class Player {
-
-  constructor(name, status, totalCoins, hasStar) {
+  constructor(name, status = "Big", totalCoins = 0, hasStar = false) {
     this.name = name;
     this.status = status;
     this.totalCoins = totalCoins;
@@ -22,24 +9,65 @@ class Player {
   }
 
   setName() {
-  if (pickPlayer === "m" || pickPlayer === "M") {
-    activePlayer.name = Mario
-  }
-  else if (pickPlayer === "l" || pickPlayer === "L") {
-    activePlayer.name = Luigi
-  }
-  else {
-    console.log("That wasn't an option");
-  }
+    if (pickPlayer === "m" || pickPlayer === "M") {
+      this.name = "Mario";
+    } else if (pickPlayer === "l" || pickPlayer === "L") {
+      this.name = "Luigi";
+    } else {
+      console.log("That wasn't an option");
+    }
   }
 
-//   gotHit() {}
+    gotHit() {
 
-//   gotPowerUp() {}
+    }
 
-//   addCoin() {}
+    gotPowerUp() {
+
+    }
+
+    addCoin() {
+
+    }
 
   print() {
-    console.log(`Name: ${this.name}\nStatus: ${this.status}\n${this.totalCoins}`);
+    console.log(
+      `Name: ${this.name}\nStatus: ${this.status}\nTotal Coins: ${this.totalCoins}`
+    );
   }
+
 }
+
+//pick player question
+const pickPlayer = readline.keyIn(
+  "Hello, would you like to be 'm' = Mario or 'l' = Luigi? "
+);
+
+// instantiate player
+const activePlayer = new Player();
+activePlayer.setName(pickPlayer);
+
+// console.log(activePlayer.name)
+
+
+let randomInteger (min, max) => {
+  let num = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  if (num === 0) {
+    gotHit()
+  }
+  else if (num === 1) {
+    gotPowerUp()
+  }
+  else if (num === 2) {
+    addCoin()
+  }
+
+
+}
+
+
+
+
+
+// activePlayer.print()
