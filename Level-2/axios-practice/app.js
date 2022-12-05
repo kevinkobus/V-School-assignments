185a7ecabd947c8f166b476e4a0096f440c9dcd4
Kevin Kobus <kevinkobus@gmail.com>
2022-12-04 18:40:36 -0700

    started practicing axios requests

--- Level-2/axios-practice/app.js
+++ Level-2/axios-practice/app.js
@@ -1,1 +1,9 @@
 const axios = require("axios")
+
+function assignResponseToVariable(response){
+    console.log(response.data);
+}
+
+axios.get('https://swapi.dev/api/people/1').then(assignResponseToVariable)
+
+
