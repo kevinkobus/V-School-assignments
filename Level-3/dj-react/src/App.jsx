ee2bd4ecad5e1f03c9c065b551092cadbdd7d741
Kevin Kobus <kevinkobus@gmail.com>
2023-02-20 12:21:38 -0700

    saved code from scrimba forms lessons

--- Level-3/dj-react/src/App.jsx
+++ Level-3/dj-react/src/App.jsx
@@ -6,13 +6,22 @@ import "./App.css";
 function App() {
   const [squares, setSquares] = React.useState(data);
 
-  const squareElements = squares.map((square) => (
-    <Box key={square.id} on={square.on} />
+  function toggle(id) {
+    console.log(id)
+  }
+
+  const squareElements = squares.map(square => (
+    <Box 
+    key={square.id} 
+    id={square.id}
+    on={square.on}
+    toggle={toggle} 
+    />
   ));
 
-  return <main className="box-container">{squareElements}
-            
-          </main>;
+  return <main className="box-container">
+            {squareElements}
+        </main>;
 }
 
 export default App;
--- Level-3/dj-react/src/components/Box.jsx
+++ Level-3/dj-react/src/components/Box.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 
 function Box(props) {
-  const [on, setOn] = React.useState(props.on)
 
   const styles = {
-    backgroundColor: on ? "#000000" : "transparent"
+    backgroundColor: props.on ? "#000000" : "transparent"
   }
 
-  function toggle() {
-    setOn(prevOn => !prevOn)
-  }
-
-  return <div style={styles} className="box">
-          <button onClick={toggle}>Small time DJ</button>
+  return <div 
+            style={styles} 
+            className="box"
+            onClick={()=>props.toggle(props.id)}
+          >
         </div>;
 }
 
