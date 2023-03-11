4c061739ddafe7d3f98c7836ae94691e76a77e5d
Kevin Kobus <kevinkobus@gmail.com>
2023-03-11 10:14:22 -0700

    done with random color component assignment except for one bug

--- Level-3/random-color/src/components/Color.jsx
+++ Level-3/random-color/src/components/Color.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import {useState, useEffect } from "react";
+import axios from "axios"
 
 function Color() {
-  return <main>something here</main>;
+    const [color, setColor] = useState("")
+    const [randomColor, setRandomColor] = useState("")
+
+    // https://www.colr.org/json/color/random?timestamp=${new Date().getTime()}
+
+useEffect(()=> {
+    axios
+    .get("https://www.colr.org/json/color/random?timestamp=${new Date().getTime()}")
+    .then((response) => setColor(response.data.colors[0].hex))
+    .catch((error) => console.log(error));
+}, [])
+
+function clickNewColor() {
+    setRandomColor(color)
+}
+
+  return (
+    <main className="border">
+      <div className="color-container" style={{backgroundColor: `#${randomColor}`}}></div>
+      <button onClick={clickNewColor}>Change Color</button>
+    </main>
+  );
 }
 
 export default Color;
