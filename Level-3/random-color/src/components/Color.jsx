81290a1d080791b6d806da9c005423d8c9934bdf
Kevin Kobus <kevinkobus@gmail.com>
2023-03-12 14:25:28 -0600

    still one bug in random color component exercise

--- Level-3/random-color/src/components/Color.jsx
+++ Level-3/random-color/src/components/Color.jsx
@@ -1,26 +1,32 @@
-import {useState, useEffect } from "react";
+import React, {useState, useEffect } from "react";
 import axios from "axios"
 
 function Color() {
     const [color, setColor] = useState("")
-    const [randomColor, setRandomColor] = useState("")
+    const [randomColor, setRandomColor] = useState(false)
 
     // https://www.colr.org/json/color/random?timestamp=${new Date().getTime()}
 
-useEffect(()=> {
+  useEffect(()=> {
     axios
     .get("https://www.colr.org/json/color/random?timestamp=${new Date().getTime()}")
     .then((response) => setColor(response.data.colors[0].hex))
     .catch((error) => console.log(error));
-}, [])
+}, [randomColor])
+
+console.log(color)
 
 function clickNewColor() {
-    setRandomColor(color)
+     setRandomColor(prevRandomColor => !prevRandomColor) 
+}
+
+const background = {
+  backgroundColor: `#${color}`
 }
 
   return (
     <main className="border">
-      <div className="color-container" style={{backgroundColor: `#${randomColor}`}}></div>
+      <div className="color-container" style={background}></div>
       <button onClick={clickNewColor}>Change Color</button>
     </main>
   );
