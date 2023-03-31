d3e3338e81cbe586ba43c54982ca0dd7fe8d71bc
Kevin Kobus <kevinkobus@gmail.com>
2023-03-31 16:43:51 -0600

    completed css gradient generator

--- Level-4/css-gradient-generator/src/App.jsx
+++ Level-4/css-gradient-generator/src/App.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import { useState } from "react";
 
 function App() {
+  const [color, setColor] = useState({
+    color1: "#ff0000",
+    color2: "#0000ff",
+    angle: "50",
+  });
 
+function handleChange(e) {
+  const {name, value}= e.target
+  setColor(prevColor => {
+    return {
+      ...prevColor, [name]: value
+    }
+  })
+}
 
+const colorCode = `background: linear-gradient(${color.angle}deg, ${color.color1}, ${color.color2})
+-moz-background: linear-gradient(${color.angle}deg, ${color.color1}, ${color.color2})
+-webkit: linear-gradient(${color.angle}deg, ${color.color1}, ${color.color2})`
+
+const colorGradient = `linear-gradient(${color.angle}deg, ${color.color1}, ${color.color2})`
 
   return (
     <main className="background">
@@ -15,40 +33,42 @@ function App() {
             className="color-input"
             type="color"
             name="color1"
-           
-            
+            placeholder="Color 1"
+            value={color.color1}
+            onChange={handleChange}
           />
           <h3>Color 2:</h3>
           <input
             className="color-input"
             type="color"
             name="color2"
-           
-            
+            placeholder="Color 2"
+            value={color.color2}
+            onChange={handleChange}
           />
           <h3>Angle:</h3>
           <input
             className="angle-input"
-            
+            name="angle"
+            type="number"
+            min="0"
+            max="180"
+            value={color.angle}
+            onChange={handleChange}
           />
           <h3>Gradient Code:</h3>
-          <textarea 
-          readOnly
-          className="gradient-code" 
-          value="Text area text goes here" 
+          <textarea
+            readOnly
+            className="gradient-code"
+            value={colorCode}
           />
-
         </form>
       </div>
-      <div className="gradient-display"></div>
+      <div
+        className="gradient-display"
+        style={{background: colorGradient}}>
+      </div>
     </main>
-
-    //     {/* <textarea className="colorBox" style=background: linear-gradient(50deg, #FFFF00 , #000000);
-
-    //       -moz-background: linear-gradient(50deg, #FFFF00 , #000000);
-
-    //       -webkit: linear-gradient(50deg, #FFFF00 , #000000)
-    //       /> */}
   );
 }
 
