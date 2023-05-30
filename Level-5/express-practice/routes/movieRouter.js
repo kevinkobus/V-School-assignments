6878fbe93e6545c14954695752473044535a0d46
Kevin Kobus <kevinkobus@gmail.com>
2023-05-30 15:55:38 -0600

    finished more express server practice

--- Level-5/express-practice/routes/movieRouter.js
+++ Level-5/express-practice/routes/movieRouter.js
@@ -40,6 +40,24 @@ movieRouter.post("/", (req, res) => {
   res.send(`Successfully added ${newMovie.title} to the database!`);
 });
 
+// Delete One
+movieRouter.delete("/:movieId", (req, res) => {
+  const movieId = req.params.moviedId
+  const movieIndex = movies.findIndex(movie => movie._id === movieId)
+  movies.splice(movieIndex, 1)
+  res.send("Succesfully deleted movie!")
+})
+
+// Update One
+movieRouter.put("/:movieId", (req, res) => {
+  movieId = req.params.movieId
+  const updateObject = req.body
+  const movieIndex = movies.findIndex(movie => movie._id === movieId)
+  const updatedMovie = Object.assign(movies[movieIndex], updateObject)
+  res.send(updatedMovie)
+})
+
+
 // Option 2
 // movieRouter
 //   .route("/")
