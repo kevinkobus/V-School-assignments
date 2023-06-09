f5eafd09bd5c0a681001c1a3d58437ed94193e14
Kevin Kobus <kevinkobus@gmail.com>
2023-06-09 13:11:52 -0600

    completed bounty hunter part 3

--- Level-5/bounty-hunter/client/src/components/BountyCard.jsx
+++ Level-5/bounty-hunter/client/src/components/BountyCard.jsx
@@ -2,22 +2,22 @@ import React, { useState } from "react";
 import BountyForm from "./BountyForm";
 
 function BountyCard(props) {
-  // console.log(props._id)
   const [editToggle, setEditToggle] = useState(false);
+  const { headShot, firstName, lastName, status, bounty, type, _id } = props;
 
   return (
     <div className="card-container">
       {!editToggle ? (
         <>
           <div className="pic-name">
-            <img src={props.headShot} alt="image" className="head-shot" />
-            <h3>First Name: {props.firstName}</h3>
-            <h3>Last Name: {props.lastName}</h3>
+            <img src={headShot} alt="image" className="head-shot" />
+            <h3>First Name: {firstName}</h3>
+            <h3>Last Name: {lastName}</h3>
           </div>
           <div className="info">
-            <h4>Status: {props.status}</h4>
-            <h4>Bounty: ${props.bounty}</h4>
-            <h4>Family: {props.type}</h4>
+            <h4>Status: {status}</h4>
+            <h4>Bounty: ${bounty}</h4>
+            <h4>Family: {type}</h4>
           </div>
           <div className="buttons">
             <button
@@ -42,7 +42,7 @@ function BountyCard(props) {
             lastName={lastName}
             status={status}
             bounty={bounty}
-            family={family}
+            family={type}
             _id={_id}
             btnText="Submit Edit"
             submit={props.editBounty}
