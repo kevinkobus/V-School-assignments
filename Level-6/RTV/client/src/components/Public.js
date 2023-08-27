13a0e776e13779b11e61d4fbc3960f7995523424
Kevin Kobus <kevinkobus@gmail.com>
2023-08-26 18:26:00 -0600

    more progress on RTV assignment

--- Level-6/RTV/client/src/components/Public.js
+++ Level-6/RTV/client/src/components/Public.js
@@ -1,9 +1,14 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import Issue from './Issue.js'
 import { IssuesContext } from "../context/IssuesContext"
 
 function Public(props){
-  const { publicIssues } = useContext(IssuesContext)
+  const { publicIssues, getPublicIssues } = useContext(IssuesContext)
+
+  useEffect(() => {
+    getPublicIssues()
+  }, [])
+
   return (
     <div className="public">
       <h1>All Issues</h1>
